6ab0e4f6333899a0b8b6ad94e44b339eebd79ef8
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-11-06 20:57:16 -0700

    Navbar done

---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+export default function NavBar() {
+   return (
+    <header className="bg-orange-600">
+        <div className="container mx-auto flex justify-between">
+            <nav className="flex">
+                <NavLink to= "/" 
+                activeClassName = "text-white"
+                className="inflex-flex items-center py-6 px-0 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">  Shivan Mahendrarajah </NavLink>
+                <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" 
+                 activeClassName="text-red-100 bg-red-700">  Blog Posts </NavLink>
+                <NavLink to = "/project" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800" 
+                activeClassName="text-red-100 bg-red-700">  Projects </NavLink>
+                <NavLink to = "/about" className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                activeClassName="text-red-100 bg-red-700">  About Me </NavLink>
+            </nav>
+            <div className="inline-flex py-3 px-3 my-6">
+                <SocialIcon url = "twitter.com" className="mr-4" target="_blank" fgColor = "#fff" style={{height: 35, width: 35}}/>
+                <SocialIcon url = "instagram.com" className="mr-4" target="_blank" fgColor = "#fff" style={{height: 35, width: 35}}/>
+                <SocialIcon url = "linkedin.com" className="mr-4" target="_blank" fgColor = "#fff" style={{height: 35, width: 35}}/>
+            </div>
+        </div>
+    </header>
+   )
+}
