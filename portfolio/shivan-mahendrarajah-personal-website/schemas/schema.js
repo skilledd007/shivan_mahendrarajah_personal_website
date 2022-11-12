d1ea628d2ad266b40661708274a2fde46049c989
Amrit Mahendrarajah <amritm3000@gmail.com>
2022-11-11 20:40:45 -0700

    Created database for travelogue

--- portfolio/shivan-mahendrarajah-personal-website/schemas/schema.js
+++ portfolio/shivan-mahendrarajah-personal-website/schemas/schema.js
@@ -6,10 +6,10 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
 import blockContent from './blockContent'
-import category from './category'
 import post from './post'
 import author from './author'
-
+import project from './project'
+import travelogue from './travelogue'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -21,7 +21,8 @@ export default createSchema({
     // in the studio.
     post,
     author,
-    category,
+    project,
+    travelogue,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
---
+++ portfolio/shivan-mahendrarajah-personal-website/schemas/travelogue.js
@@ -0,0 +1,23 @@
+export default {
+    name: "travelogue",
+    title: "Travelogue",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            type: "string",
+        },
+        {
+            name: "date",
+            type: "datetime",
+        } ,
+        {
+            name: "place",
+            type: "string"
+        } ,
+        {
+            name: "post",
+            type: "text",
+        } ,
+    ]
+}
