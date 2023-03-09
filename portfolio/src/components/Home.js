3f1c5c1a5b90d3f79e658aa4d28ec3fc0ab2ad13
Amrit Mahendrarajah <amritm3000@gmail.com>
2023-03-09 16:13:05 -0700

    Test update

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
         <main>
             <img src={image} alt="background of Iran" className="absolute object-covered w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"> Hi, I'm Shivan. </h1>
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name"> Hi, I'm Shivann. </h1>
             </section>
         </main>
     );
